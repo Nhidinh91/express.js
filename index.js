87ae2716181f6e58e02f8fd36fb55f78e4635d3f
NhiDinh <thaonhi6991@gmail.com>
2024-08-22 20:28:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/home', (req, res) => {
+    res.send("Welcome to our page");
+});
+
+app.get('/about', (req, res) => {
+    res.send("About us");
+});
+
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+});
